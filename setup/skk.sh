d50e5a2224c5bcfe96e0d1e869d61f2fce343652
yokenzan <31175068+yokenzan@users.noreply.github.com>
2019-02-23 16:23:29 +0900

    インストーラ?ビルダ?を作成

--- setup/skk.sh
+++ setup/skk.sh
@@ -3,7 +3,7 @@
 rm   -rf $HOME/.skk
 mkdir -p $HOME/.skk
 
-if [ $(curl http://openlab.jp/skk/dic/SKK-JISYO.L.gz) ]; then
+if [ $(curl -s http://openlab.jp/skk/dic/SKK-JISYO.L.gz) ]; then
     curl -ko $HOME/.skk/SKK-JISYO.L.gz http://openlab.jp/skk/dic/SKK-JISYO.L.gz
     gunzip   $HOME/.skk/SKK-JISYO.L.gz
 else
