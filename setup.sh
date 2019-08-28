a42703dcc92e92817dc1399a51fb3f2e78eb6df3
yokenzan <31175068+yokenzan@users.noreply.github.com>
2019-08-28 12:48:55 +0900

    Bash : fix bug of setup script

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ ln -sf $HOME/.dotfiles/.bashrc           $HOME
 ln -sf $HOME/.dotfiles/.bash_profile     $HOME
 ln -sf $HOME/.dotfiles/.bash_aliases     $HOME
 ln -sf $HOME/.dotfiles/.bash_functions   $HOME
-ln -sf /home/cyrt/.dotfiles/.Xresources  $HOME
+ln -sf $HOME/.dotfiles/.Xresources       $HOME
 
 ln -sf $HOME/.dotfiles/.inputrc          $HOME
 ln -sf $HOME/.dotfiles/.editrc           $HOME
@@ -26,8 +26,8 @@ ln -sf $HOME/.dotfiles/.ctags            $HOME
 ln -sf $HOME/.dotfiles/.i3status.conf    $HOME
 ln -sf $HOME/.dotfiles/.tigrc            $HOME
 
-ln -sf $HOME/.dotfiles/.emacs                             $HOME/.emacs.d/init.el
-ln -sf $HOME/.dotfiles/composer.json                      $HOME/.config/composer
+ln -sf $HOME/.dotfiles/.emacs            $HOME/.emacs.d/init.el
+ln -sf $HOME/.dotfiles/composer.json     $HOME/.config/composer
 
 ln -sf $HOME/.dotfiles/.config/i3/config                  $HOME/.config/i3
 ln -sf $HOME/.dotfiles/.config/alacritty/alacritty.yml    $HOME/.config/alacritty
@@ -38,10 +38,11 @@ ln -sf $HOME/.dotfiles/.config/ranger/rc.conf             $HOME/.config/ranger
 ln -sf $HOME/.dotfiles/.config/ranger/rifle.conf          $HOME/.config/ranger
 ln -sf $HOME/.dotfiles/.config/ranger/scope.sh            $HOME/.config/ranger
 
-ln -sf /home/onuki/.dotfiles/.config/gtk-3.0/settings.ini $HOME/.config/gtk-3.0
+ln -sf $HOME/.dotfiles/.config/gtk-3.0/settings.ini       $HOME/.config/gtk-3.0
 
-ln -sf /home/cyrt/.dotfiles/.mlterm/aafont                $home/.mlterm
-ln -sf /home/cyrt/.dotfiles/.mlterm/main                  $home/.mlterm
+ln -sf $HOME/.dotfiles/.mlterm/aafont                     $HOME/.mlterm
+ln -sf $HOME/.dotfiles/.mlterm/main                       $HOME/.mlterm
+ln -sf $HOME/.dotfiles/.mlterm/color                      $HOME/.mlterm
 
 
 if [ $(uname | grep -qe 'NT') ]; then
@@ -60,9 +61,7 @@ for i in $(find $HOME/.dotfiles/setup -type f); do $i; done
 chmod -R 777 $HOME/.dotfiles/build.sh
 $HOME/.dotfiles/build.sh
 
-if [ $(env | grep -ie ubuntu) ]; then
-    mkdir $HOME/.themes
-    git clone git://github.com/debug-ito/GtkEmacs-debug-ito.git $HOME/.themes
-    gsettings set org.gnome.desktop.interface gtk-key-theme GtkEmacs-debug-ito
-fi
+mkdir $HOME/.themes
+git clone --depth=1 git://github.com/debug-ito/GtkEmacs-debug-ito.git $HOME/.themes/GtkEmacs-debug-ito
+gsettings set org.gnome.desktop.interface gtk-key-theme GtkEmacs-debug-ito
 
