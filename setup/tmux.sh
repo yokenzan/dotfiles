0dcc912a9d77939f09f3fb7a7450bdf7ec96d5f2
yokenzan <31175068+yokenzan@users.noreply.github.com>
2019-11-04 14:33:12 +0900

    Tmux : remove config files of each OS

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -3,15 +3,7 @@
 rm -rf   $HOME/.tmux
 
 mkdir -p $HOME/.tmux/plugins
-ln -sf   $HOME/.dotfiles/.tmux.conf         $HOME
-
-
-if [ $(uname | grep -qe 'NT') ]; then
-    # Windows
-    ln -sf $HOME/.dotfiles/.tmux.conf.msys  $HOME
-else
-    ln -sf $HOME/.dotfiles/.tmux.conf.unix 	$HOME
-fi
+ln -sf   $HOME/.dotfiles/.tmux.conf $HOME
 
 
 git clone git://github.com/tmux-plugins/tpm.git $HOME/.tmux/plugins/tpm
