222bf640163cd4577a0927c17df2bd5014908ac4
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-06-01 10:15:18 +0900

    Tmux : update Tmux version when building environment

--- build/tmux.sh
+++ build/tmux.sh
@@ -3,8 +3,8 @@
 : ========== Installing TMUX.. ==========
 
 cd $HOME/wk/oss
-curl -OL https://github.com/tmux/tmux/releases/download/3.1/tmux-3.1-rc4.tar.gz
-tar xvzf tmux-3.1-rc4.tar.gz
-cd tmux-3.1-rc4 && ./configure && make && sudo make install
+curl -OL https://github.com/tmux/tmux/releases/download/3.1/tmux-3.1.tar.gz
+tar xvzf tmux-3.1.tar.gz
+cd tmux-3.1 && ./configure && make && sudo make install
 
 : ========== Completed Installing TMUX ==========
